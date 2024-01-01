82d12e0109655920a1bcf4dff6c5ca04a4f1239a
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-01-01 21:37:33 +0200

    Task2 - All Albums Produced by Given Producer Add method string ExportAlbumsInfo(MusicHubDbContext context, int producerId) in the StartUp class that receives a ProducerId. Export all albums which are produced by the provided ProducerId. For each Album, get the Name, ReleaseDate in format the "MM/dd/yyyy", ProducerName, the Album Songs with each Song Name, Price (formatted to the second digit) and the Song WriterName. Sort the Songs by Song Name (descending) and by Writer (ascending). At the end export the Total Album Price with exactly two digits after the decimal place. Sort the Albums by their Total Price (descending).

--- MusicHub/StartUp.cs
+++ MusicHub/StartUp.cs
@@ -1,7 +1,8 @@
 ﻿namespace MusicHub
 {
     using System;
-
+    using System.Globalization;
+    using System.Text;
     using Data;
     using Initializer;
 
@@ -15,12 +16,52 @@
             DbInitializer.ResetDatabase(context);
 
             //Test your solutions here
+            Console.WriteLine(ExportAlbumsInfo(context, 9));
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            var albums = context.Albums
+                .Where(a => a.ProducerId.HasValue && a.ProducerId == producerId)//.ToArray()
+                //.OrderByDescending(a=>a.Price)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs.Select(s => new
+                    {
+                        SongName = s.Name,
+                        Price = s.Price,//$"{s.Price:f2}",
+                        Writer = s.Writer.Name
+                    })
+                    .OrderByDescending(s => s.SongName)
+                    .ThenBy(s => s.Writer).ToArray(),
+                    AlbumPrice = a.Price//$"{a.Price:f2}"
+                }).ToArray();
+          
+            foreach ( var album in albums.OrderByDescending(a => a.AlbumPrice))
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine("-Songs:");
+                int songNumber = 0;
+                foreach (var song in album.Songs)
+                {
+                    songNumber++;
+                    sb.AppendLine($"---#{songNumber}");
+                    sb.AppendLine($"---SongName: {song.SongName}");
+                    sb.AppendLine($"---Price: {song.Price:f2}");
+                    sb.AppendLine($"---Writer: {song.Writer}"); 
+                }
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+            return sb.ToString().TrimEnd();       
         }
+
+        
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
